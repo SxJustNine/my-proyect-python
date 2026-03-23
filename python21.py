6a539b0d8a6e08d2f259de8e2c90051b0c1eaa70
SxJustNine <149451876+SxJustNine@users.noreply.github.com>
2026-03-22 22:34:10 -0300

    Add files via upload

---
+++ python21.py
@@ -0,0 +1,43 @@
+#DECORACION
+print("=" * 30)
+print("DATOS".center(30))
+print("=" * 30)
+
+def login():
+    while True:
+        nombre = input("Cual es tu nombre: ")
+        if nombre.isalpha():
+            break
+        else:
+            print("Solo letras!")
+    while True:
+        try:
+            edad = int(input("Cual es tu edad: "))
+            if edad >=18:
+                categoria = "+18"
+            elif edad < 18:
+                print("=" * 35)
+                print("Eres menor de edad, No puedes pasar!".center(35))
+                print("=" * 35)
+                exit()
+            break
+        except ValueError:
+            print("No letras!")
+    while True:
+        juego = input("Cual es tu juego favorito: ")
+        if juego.isalpha():
+            break
+        else:
+            print("Solo letras!")
+
+    return nombre, edad, categoria, juego
+
+nombre, edad, categoria, juego = login()
+# decorativo
+print("=" * 30)
+print("INFORMACION".center(30))
+print("=" * 30)
+print(f"· Nombre: {nombre}")
+print(f"· Edad: {edad} ({categoria})")
+print(f"Juego Fav: {juego}")
+
